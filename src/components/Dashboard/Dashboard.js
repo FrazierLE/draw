0bb214400f35db9219fe5448a647735bc6410704
FrazierLE <lauren.e.frazier@gmail.com>
2023-06-29 13:12:25 -0600

    Add New User functionality

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Card, Button } from '@mui/material'
+import UserFolder from '../UserFolder/UserFolder'
+import AddIcon from '@mui/icons-material/Add';
 
 const Dashboard = () => {
+  const [users, setUsers] = useState([])
+
+  const addNewUser = (e) => {
+    e.preventDefault()
+    const newUser = {
+      id: Date.now()
+    }
+    setUsers([...users, newUser])
+  }
+
+
   return (
-    <div>
-      Dashboard
-    </div>
+    <Box>
+      <Button onClick={(e) => addNewUser(e)}><AddIcon />Add New User</Button>
+      {/* <UserFolder /> */}
+    </Box>
   )
 }
 
---
+++ src/components/UserFolder/UserFolder.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FolderIcon from '@mui/icons-material/Folder';
+
+const UserFolder = () => {
+  return(
+    <FolderIcon />
+  )
+}
+
+export default UserFolder 
