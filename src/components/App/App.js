69684d419d43c4d490fc689aa473da4b43a9fcd5
FrazierLE <lauren.e.frazier@gmail.com>
2023-06-28 21:01:37 -0600

    Create Dashboard component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,23 @@ import Draw from '../Draw/Draw';
 import NavBar from '../NavBar/NavBar';
 import Login from '../Login/Login';
 import SavedSection from '../SavedSection/SavedSection';
+import Dashboard from '../Dashboard/Dashboard';
 import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-      <div>
+    <Routes>
+      <Route path='/' element={<Login />}/>
+      <Route path='/digistrip' 
+        element={<div>
         <NavBar />
         <Draw />
         <SavedSection />
-      </div>
+      </div>}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+    </Routes>
     </div>
   );
 }
