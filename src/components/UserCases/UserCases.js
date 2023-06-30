30ca3dde0eb9b298d7991988b7e04e96735b6706
FrazierLE <lauren.e.frazier@gmail.com>
2023-06-30 15:13:48 -0600

    Match user to id before displaying cases

--- src/components/UserCases/UserCases.js
+++ src/components/UserCases/UserCases.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import FolderIcon from '@mui/icons-material/Folder';
-import { Card, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { NavLink, useParams, useOutletContext } from 'react-router-dom';
 import Case from '../Case/Case';
 
 const UserCases = () => {
   const params = useParams()
   const context = useOutletContext()
+  const match = context.filter(item => item.id === Number(params.id))
 
-  const list = context[0].cases.map(item => {
+  const list = match[0].cases?.map(item => {
     return(
       <Case 
         key={item.id}
@@ -21,9 +22,12 @@ const UserCases = () => {
   })
 
   return(
-    <div>
-      {list}
-    </div>
+    <Box>
+      <Typography variant='h3'>
+        User Cases
+      </Typography>
+      {list ? <div>{list}</div> : <h3>This User has no cases yet.</h3>}
+    </Box>
   )
 }
 
